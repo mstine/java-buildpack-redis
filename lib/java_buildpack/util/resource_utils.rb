8c482208f0dfb12b75b5b6d4165111e63a91c1a0
Matt Stine <mstine@gopivotal.com>
2013-11-22 08:41:07 -0600

    rescue ERB error

--- lib/java_buildpack/util/resource_utils.rb
+++ lib/java_buildpack/util/resource_utils.rb
@@ -53,7 +53,11 @@ module JavaBuildpack::Util
     # @param [String] target_file the name of the file to write
     def self.generate_bound_resource_from_template(template, service, target_directory, target_file)
       template_file = File.open(File.join(get_resources('templates'), template), 'r').read
-      erb = ERB.new(template_file)
+      begin
+        erb = ERB.new(template_file)
+      rescue Error => e
+        puts e.message
+      end
       puts "Before open..."
       File.open(File.join(target_directory, target_file), 'w+') do |file|
         file.write(erb.result(binding))
